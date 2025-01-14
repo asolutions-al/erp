527930a87a915a4d4839954d2387b760ac901d17
anxhi <anxhirr@gmail.com>
2025-01-14 19:18:16 +0100

    fix: remove unit validator as it breaks static route

--- app/[unitId]/layout.tsx
+++ app/[unitId]/layout.tsx
@@ -1,38 +1,11 @@
 import { AppHeader } from "@/components/layout/app-header"
 import { AppSidebarNew } from "@/components/layout/app-sidebar"
 import { SidebarProvider } from "@/components/ui/sidebar"
-import { createAuthClient } from "@/db/(auth)/client"
-import { db } from "@/db/(inv)/instance"
-import { member } from "@/orm/(inv)/schema"
-import { eq } from "drizzle-orm"
-import { redirect } from "next/navigation"
-import { PropsWithChildren, Suspense } from "react"
+import { PropsWithChildren } from "react"
 
-type Props = PropsWithChildren<{
-  params: Promise<{ unitId: string }>
-}>
+type Props = PropsWithChildren<{}>
 
-const UnitValidator = async ({ unitId }: { unitId: string }) => {
-  const client = await createAuthClient()
-  const {
-    data: { user },
-  } = await client.auth.getUser()
-  const userId = user!.id
-  const members = await db.query.member.findMany({
-    where: eq(member.userId, userId),
-    with: { unit: true },
-  })
-  const unitsList = members.map((member) => member.unit)
-  const unitIsValid = unitsList.some((unit) => unit.id === unitId)
-
-  if (!unitIsValid) return redirect(`/units`)
-
-  return null
-}
-
-const Layout = async ({ children, params }: Props) => {
-  const { unitId } = await params
-
+const Layout = async ({ children }: Props) => {
   return (
     <SidebarProvider>
       <AppSidebarNew />
@@ -40,9 +13,6 @@ const Layout = async ({ children, params }: Props) => {
         <AppHeader />
         <div className="flex-1 m-1.5 md:m-2 lg:m-2.5">{children}</div>
       </main>
-      <Suspense>
-        <UnitValidator unitId={unitId} />
-      </Suspense>
     </SidebarProvider>
   )
 }
