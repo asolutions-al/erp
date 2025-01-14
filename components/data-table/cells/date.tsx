e5599e8297dd4b6a38d43ea06e54faefe492206a
anxhi <anxhirr@gmail.com>
2025-01-14 19:29:24 +0100

    fix: data table comp

--- components/data-table/cells/date.tsx
+++
@@ -1,6 +0,0 @@
-import dayjs from "dayjs"
-
-export const DateCell = ({ date }: { date: string }) => {
-  const formattedDate = dayjs(date).format("DD/MM/YYYY:HH:mm")
-  return <span>{formattedDate}</span>
-}
--- components/data-table/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./cells"
-export * from "./column-header"
-export * from "./column-toggle"
-export * from "./data-table"
-export * from "./pagination"
