a31cff729861aaf298fe106615306c863a9a4a74
anxhi <anxhirr@gmail.com>
2025-01-26 22:21:40 +0100

    feat: bakdrop blur on header

--- app/org/[orgId]/[unitId]/layout.tsx
+++ app/org/[orgId]/[unitId]/layout.tsx
@@ -9,8 +9,8 @@ import { PropsWithChildren, Suspense } from "react"
 
 import { SidebarMain } from "@/components/layout/sidebar-main"
 import { SidebarUser } from "@/components/layout/sidebar-user"
-import { Separator } from "@/components/ui/separator"
 import Image from "next/image"
+import Link from "next/link"
 
 type Props = PropsWithChildren<{
   params: Promise<{ orgId: string; unitId: string }>
@@ -39,16 +39,18 @@ const Layout = async (props: Props) => {
         </Suspense>
         <SidebarRail />
       </Sidebar>
-      <main className="relative flex min-h-svh flex-1 flex-col overflow-x-auto">
-        <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Image src="/logo.png" alt="logo" width={30} height={30} />
-          <Separator orientation="vertical" className="mx-2 h-4" />
-        </header>
+      <div className="flex-1">
+        <header className="sticky top-0 z-10 flex h-16 items-center border-b px-2 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+          <Link href={`/org/${orgId}/~/list`}>
+            <Image src="/logo.png" alt="logo" width={30} height={30} />
+          </Link>
 
-        <div className="m-1.5 flex-1 md:m-2 lg:m-2.5">{children}</div>
-      </main>
+          <div className="ml-auto md:hidden">
+            <SidebarTrigger />
+          </div>
+        </header>
+        <main className="m-1.5 flex-1 md:m-2 lg:m-2.5">{children}</main>
+      </div>
     </SidebarProvider>
   )
 }
