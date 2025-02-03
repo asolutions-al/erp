79b1fe35a6734237c6cc7c67a9ab7ae17114fc6c
anxhi <anxhirr@gmail.com>
2025-02-03 20:45:30 +0100

    chore: remove unused code

--- components/columns/org.tsx
+++ components/columns/org.tsx
@@ -4,41 +4,8 @@ import { SortBtn } from "@/components/buttons"
 import { OrgSchemaT } from "@/db/app/schema"
 import { ColumnDef } from "@tanstack/react-table"
 
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { CellContext } from "@tanstack/react-table"
-import { EditIcon, MoreHorizontalIcon } from "lucide-react"
-import { useTranslations } from "next-intl"
-
 type SchemaT = OrgSchemaT & {
   unitsCount: number
-}
-
-const Actions = ({ row }: CellContext<SchemaT, unknown>) => {
-  const t = useTranslations()
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="h-8 w-8 p-0">
-          <span className="sr-only">Open menu</span>
-          <MoreHorizontalIcon className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        <DropdownMenuItem>
-          <EditIcon />
-          {t("Manage")}
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
 }
 
 const columns: ColumnDef<SchemaT>[] = [
