b61932f93e3ce9a9334d1f664e6fa48ff29c8285
anxhi <anxhirr@gmail.com>
2025-02-10 21:19:51 +0100

    fix: sort icon

--- components/buttons/sort.tsx
+++ components/buttons/sort.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button"
-import { Column, RowData } from "@tanstack/react-table"
-import { ArrowUpDownIcon } from "lucide-react"
+import { Column, RowData, SortDirection } from "@tanstack/react-table"
+import { ArrowDownIcon, ArrowUpDownIcon, ArrowUpIcon } from "lucide-react"
 import { Messages, useTranslations } from "next-intl"
+
+const ICON_MAP: Record<SortDirection, React.ComponentType> = {
+  asc: ArrowUpIcon,
+  desc: ArrowDownIcon,
+}
 
 export const SortBtn = <TData extends RowData>({
   text,
@@ -12,13 +17,14 @@ export const SortBtn = <TData extends RowData>({
 }) => {
   const t = useTranslations()
 
+  const isSorted = column.getIsSorted()
+
+  const Icon = isSorted ? ICON_MAP[isSorted] : ArrowUpDownIcon
+
   return (
-    <Button
-      variant="ghost"
-      onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-    >
+    <Button variant="ghost" onClick={() => column.toggleSorting()}>
       {t(text)}
-      <ArrowUpDownIcon className="ml-2 h-4 w-4" />
+      <Icon className="ml-2 h-4 w-4" />
     </Button>
   )
 }
