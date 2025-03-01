ab68bffeef088b552778380a688d598a188bf281
anxhi <anxhirr@gmail.com>
2025-03-01 16:05:07 +0100

    feat: translate schema

--- providers/form/invoice.tsx
+++ providers/form/invoice.tsx
@@ -4,54 +4,59 @@ import { Form } from "@/components/ui/form"
 import { invoice, invoiceRow } from "@/orm/app/schema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { createInsertSchema } from "drizzle-zod"
+import { useTranslations } from "next-intl"
 import { PropsWithChildren } from "react"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
-const rowSchema = createInsertSchema(invoiceRow, {
-  productId: (sch) => sch.productId.min(1),
-  name: (sch) => sch.name.min(1),
-  price: (sch) => sch.price.min(0),
-  quantity: (sch) => sch.quantity.positive(),
-}).omit({
-  id: true,
-  createdAt: true,
-  orgId: true,
-  unitId: true,
-  invoiceId: true,
-  total: true,
-  subtotal: true,
-  tax: true,
-})
-
-const schema = createInsertSchema(invoice, {
-  customerId: (sch) => sch.customerId.min(1),
-  discountValue: (sch) => sch.discountValue.min(0),
-})
-  .omit({
+const createSchema = ({ t }: { t: ReturnType<typeof useTranslations> }) => {
+  const rowSchema = createInsertSchema(invoiceRow, {
+    productId: (sch) => sch.productId.min(1),
+    name: (sch) => sch.name.min(1),
+    price: (sch) => sch.price.min(0),
+    quantity: (sch) => sch.quantity.positive(),
+  }).omit({
     id: true,
-    unitId: true,
-    orgId: true,
     createdAt: true,
+    orgId: true,
+    unitId: true,
+    invoiceId: true,
     total: true,
     subtotal: true,
     tax: true,
   })
-  .extend({
-    rows: z.array(rowSchema).min(1),
-  })
-  .refine(
-    (data) => {
-      if (data.payMethod === "cash") return !!data.cashRegisterId
-      return true
-    },
-    {
-      path: ["cashRegisterId"],
-      message: "Cash register is required",
-    }
-  )
 
-type SchemaT = z.infer<typeof schema>
+  const schema = createInsertSchema(invoice, {
+    customerId: (sch) => sch.customerId.min(1),
+    discountValue: (sch) => sch.discountValue.min(0),
+  })
+    .omit({
+      id: true,
+      unitId: true,
+      orgId: true,
+      createdAt: true,
+      total: true,
+      subtotal: true,
+      tax: true,
+    })
+    .extend({
+      rows: z.array(rowSchema).min(1),
+    })
+    .refine(
+      (data) => {
+        if (data.payMethod === "cash") return !!data.cashRegisterId
+        return true
+      },
+      {
+        path: ["cashRegisterId"],
+        message: t("Cash register is required"),
+      }
+    )
+
+  return schema
+}
+
+type SchemaT = z.infer<ReturnType<typeof createSchema>>
 
 const defaultValues: SchemaT = {
   rows: [],
@@ -71,6 +76,10 @@ const defaultValues: SchemaT = {
 const Provider = (
   props: PropsWithChildren<{ defaultValues?: Partial<SchemaT> }>
 ) => {
+  const t = useTranslations()
+
+  const schema = createSchema({ t })
+
   const form = useForm<SchemaT>({
     resolver: zodResolver(schema),
     defaultValues: { ...defaultValues, ...props.defaultValues },
