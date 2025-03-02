5316f447d52388c67b629adc4f1b26be60ea2894
anxhi <anxhirr@gmail.com>
2025-03-02 15:56:18 +0100

    chore: remove mode from analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
               <Toaster className="print:hidden" />
             </ThemeProvider>
             <SpeedInsights />
-            <Analytics mode="production" />
+            <Analytics />
           </body>
         </TooltipProvider>
       </TranslationProvider>
