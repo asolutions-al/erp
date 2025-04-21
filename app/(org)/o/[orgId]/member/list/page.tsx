d6a08129c762f92d9aba8322698c1ef5bec47745
anxhi <anxhirr@gmail.com>
2025-04-21 16:00:45 +0200

    fix: member list page header

--- app/(org)/o/[orgId]/member/list/page.tsx
+++ app/(org)/o/[orgId]/member/list/page.tsx
@@ -1,19 +1,15 @@
 import { orgMemberColumns } from "@/components/columns/orgMember"
-import { Button } from "@/components/ui/button"
+import { PageHeader } from "@/components/layout"
 import { DataTable } from "@/components/ui/data-table"
 import { db } from "@/db/app/instance"
 import { orgMember } from "@/orm/app/schema"
 import { eq } from "drizzle-orm"
-import { PlusCircleIcon, UsersIcon } from "lucide-react"
-import { getTranslations } from "next-intl/server"
-import Link from "next/link"
 
 type Props = {
   params: Promise<{ orgId: string }>
 }
 
 const Page = async ({ params }: Props) => {
-  const t = await getTranslations()
   const { orgId } = await params
 
   const data = await db.query.orgMember.findMany({
@@ -23,19 +19,10 @@ const Page = async ({ params }: Props) => {
 
   return (
     <>
-      <div className="mb-3 flex flex-row justify-between">
-        <div className="flex items-center gap-2">
-          <UsersIcon />
-          <h1 className="text-2xl font-semibold">{t("Members")}</h1>
-        </div>
-        {/* <Link href={`/o/${orgId}/member/create`} passHref>
-          <Button>
-            <PlusCircleIcon />
-            <span className="sr-only sm:not-sr-only">{t("New member")}</span>
-          </Button>
-        </Link> */}
+      <PageHeader title="Members" className="mb-2" />
+      <div className="mx-auto max-w-4xl">
+        <DataTable columns={orgMemberColumns} data={data} />
       </div>
-      <DataTable columns={orgMemberColumns} data={data} />
     </>
   )
 }
