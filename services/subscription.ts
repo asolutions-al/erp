c63416ee0eecd574ead4dc6ef4b2bf022df37b6e
anxhi <anxhirr@gmail.com>
2025-07-10 23:31:37 +0200

    fix: handle more subscription cases

--- services/subscription.ts
+++ services/subscription.ts
@@ -5,6 +5,7 @@ import { getSubscriptionByOrgId } from "@/db/app/actions/subscription"
 import { getPlanById } from "@/db/auth/loaders"
 import { cancelPayPalSub, createPayPalSubs } from "@/lib/paypal"
 import { planId as planIdEnum } from "@/orm/app/schema"
+import { getTranslations } from "next-intl/server"
 
 const isDev = process.env.NODE_ENV === "development"
 
@@ -19,67 +20,65 @@ const createSubscription = async (
     approvalUrl: string
   }>
 > => {
-  // Check if organization already has a subscription
-  const existingSub = await getSubscriptionByOrgId(orgId)
+  const t = await getTranslations()
 
-  if (!existingSub) {
+  if (planId === "INVOICE-STARTER")
+    return {
+      success: null,
+      error: { message: "Cannot create a subscription for the free plan" },
+    }
+
+  const existing = await getSubscriptionByOrgId(orgId)
+
+  if (!existing)
     return {
       success: null,
       error: {
         message:
-          "No existing subscription found for this organization. This should not happen. Please contact support.",
+          "No subscription found for this organization. This should not happen. Please contact support.",
       },
     }
-  }
 
-  // If the organization already has an active subscription, do not allow creating a new one
-  // unless they are upgrading from the free plan
-  if (
-    existingSub.status === "ACTIVE" &&
-    existingSub.plan !== "INVOICE-STARTER"
-  ) {
+  // If the organization already has an active subscription, we cannot create a new one
+  if (existing.status === "ACTIVE" && existing.plan !== "INVOICE-STARTER")
     return {
       success: null,
       error: { message: "Organization already has an active subscription" },
     }
-  }
 
   // Get the plan details from the database to retrieve the PayPal plan ID
   const planData = await getPlanById(planId)
 
-  if (!planData) {
+  if (!planData)
     return {
       success: null,
       error: { message: "Invalid plan selected" },
     }
-  }
 
   // Create a new PayPal subscription
-  const newSubscriptionData = await createPayPalSubs(
+  const newSubRes = await createPayPalSubs(
     isDev ? planData.paypalSandboxPlanId : planData.paypalPlanId,
     orgId // Use orgId as custom_id to link PayPal subscription to your org
   )
 
-  if (!newSubscriptionData.success) {
+  if (newSubRes.error)
     return {
+      error: newSubRes.error,
       success: null,
-      error: { message: "Failed to create new subscription" },
     }
-  }
 
   // Get the approval URL for the user to approve the new subscription
-  const approvalUrl = newSubscriptionData.success.data.links?.find(
+  const approvalUrl = newSubRes.success?.data.links?.find(
     (l) => l.rel === "approve"
   )?.href
 
-  if (!approvalUrl) {
+  if (!approvalUrl)
     return {
       success: null,
       error: {
         message: "Failed to get approval URL for new subscription",
       },
     }
-  }
 
   return {
     success: {
@@ -93,34 +92,37 @@ const createSubscription = async (
  * Cancels an active subscription
  */
 const cancelSubscription = async (orgId: string): Promise<ResT<true>> => {
-  const subscription = await getSubscriptionByOrgId(orgId)
+  const t = await getTranslations()
+  const existing = await getSubscriptionByOrgId(orgId)
 
-  if (!subscription)
+  if (!existing)
     return {
       success: null,
-      error: { message: "No subscription found for this organization" },
+      error: {
+        message:
+          "No subscription found for this organization. This should not happen. Please contact support.",
+      },
     }
 
-  if (subscription.status !== "ACTIVE")
+  if (existing.status !== "ACTIVE")
     return {
       success: null,
       error: { message: "Subscription is not active" },
     }
 
-  if (subscription.plan === "INVOICE-STARTER") {
+  if (existing.plan === "INVOICE-STARTER")
     return {
       success: null,
       error: { message: "Cannot cancel a free plan" },
     }
-  }
 
-  if (!subscription.externalSubscriptionId)
+  if (!existing.externalSubscriptionId)
     return {
       success: null,
       error: { message: "No external subscription ID found" },
     }
 
-  return await cancelPayPalSub(subscription.externalSubscriptionId)
+  return await cancelPayPalSub(existing.externalSubscriptionId)
 }
 
 export { cancelSubscription, createSubscription }
