a37a7c0c2ab911d23c931dc69b9dc85ec522b3e7
anxhi <anxhirr@gmail.com>
2025-07-17 20:27:33 +0200

    chore: use light as default theme instead of system

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
           <body>
             <ThemeProvider
               attribute="class"
-              defaultTheme="system"
+              defaultTheme="light"
               enableSystem
               disableTransitionOnChange
             >
