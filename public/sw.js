d3dcb00f3c7a2fb12b3d9c4c64e8dd058a3cfebc
anxhi <anxhirr@gmail.com>
2025-07-18 11:08:45 +0200

    fix: remove useless fetch redirector

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,7 @@ self.addEventListener("activate", (event) => {
   console.log("Service Worker activating.")
 })
 
-self.addEventListener("fetch", (event) => {
-  console.log("Fetching:", event.request.url)
-  event.respondWith(fetch(event.request))
-})
+// self.addEventListener("fetch", (event) => {
+//   console.log("Fetching:", event.request.url)
+//   event.respondWith(fetch(event.request))
+// })
