a30ac756e5d3dd7fef58cb7c83b54fff0543019b
anxhi <anxhirr@gmail.com>
2025-07-18 11:18:42 +0200

    chore: test pwa without service worker

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { appUrl } from "@/contants/consts"
 import { ThemeProvider, TranslationProvider } from "@/providers"
-import { PwaProvider } from "@/providers/pwa"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GeistSans } from "geist/font/sans"
@@ -17,26 +16,24 @@ export const metadata = {
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
-    <PwaProvider>
-      <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-        <TranslationProvider>
-          <TooltipProvider>
-            <body>
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="light"
-                enableSystem
-                disableTransitionOnChange
-              >
-                {children}
-                <Toaster className="print:hidden" />
-              </ThemeProvider>
-              <SpeedInsights debug={false} />
-              <Analytics debug={false} />
-            </body>
-          </TooltipProvider>
-        </TranslationProvider>
-      </html>
-    </PwaProvider>
+    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
+      <TranslationProvider>
+        <TooltipProvider>
+          <body>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="light"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster className="print:hidden" />
+            </ThemeProvider>
+            <SpeedInsights debug={false} />
+            <Analytics debug={false} />
+          </body>
+        </TooltipProvider>
+      </TranslationProvider>
+    </html>
   )
 }
