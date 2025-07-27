7908fd96095b4d9922e4a8ac31cadc9ad1e307be
anxhi <anxhirr@gmail.com>
2025-07-27 15:40:48 +0200

    fix: build errors

--- contants/env.ts
+++ contants/env.ts
@@ -1,9 +1,9 @@
 const IS_DEV = process.env.NODE_ENV === "development"
 
-const DOMAIN = IS_DEV ? "localhost" : process.env.APP_URL
+const DOMAIN = IS_DEV ? "localhost" : process.env.APP_URL!
 
-const APP_URL = IS_DEV ? "http://localhost:3000" : process.env.APP_URL
+const APP_URL = IS_DEV ? "http://localhost:3000" : process.env.APP_URL!
 
-const AUTH_URL = IS_DEV ? "http://localhost:3001" : process.env.AUTH_URL
+const AUTH_URL = IS_DEV ? "http://localhost:3001" : process.env.AUTH_URL!
 
 export { APP_URL, AUTH_URL, DOMAIN, IS_DEV }
