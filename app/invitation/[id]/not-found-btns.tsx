fdf2f57f50bda50e7e20bc125d34e997ccfeb2e8
anxhi <anxhirr@gmail.com>
2025-08-19 19:25:39 +0200

    fix: Too many action buttons #204

--- app/invitation/[id]/not-found-btns.tsx
+++ app/invitation/[id]/not-found-btns.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import { signOut } from "@/db/auth/actions"
-import { HomeIcon, LogInIcon, UserPlusIcon } from "lucide-react"
+import { HomeIcon, LogInIcon } from "lucide-react"
 import { useTranslations } from "next-intl"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
@@ -20,17 +20,8 @@ const NotFoundBtns = () => {
         <LogInIcon />
         {t("Sign in to your account")}
       </Button>
-      <Button
-        variant="outline"
-        className="w-full"
-        size="lg"
-        onClick={() => signOut({ returnTo, page: "/signup" })}
-      >
-        <UserPlusIcon />
-        {t("Create a new account")}
-      </Button>
       <Link href="/">
-        <Button variant="ghost" className="w-full" size="lg">
+        <Button variant="outline" className="mt-1 w-full" size="lg">
           <HomeIcon />
           {t("Go to homepage")}
         </Button>
